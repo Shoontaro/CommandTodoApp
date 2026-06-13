39b28dc7217007390b53807e353a834382eb1d86
Shoontaro <tishinadw@gmail.com>
2026-06-13 14:34:07 +0300

    Добавьте файлы проекта.

---
+++ CommandTodoApp/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.CommandLine;
+using System.CommandLine.Parsing;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace CommandTodoApp;
+class Program
+{
+    private static readonly string FilePath = "todos.txt";
+
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Приложение для задачь. Добавьте задачу");
+
+       
+        GetTask();
+    }
+
+    private static void GetTask()
+    {
+        while (true)
+        {
+            string command = Console.ReadLine() ?? "";
+
+            Option<string> fileOption = new("--task")
+            {
+                Description = "Задача. Пока тестовая команда"
+            };
+
+            RootCommand rootCommand = new("Todo project with System.CommandLine");
+            rootCommand.Options.Add(fileOption);
+
+
+            //ParseResult parseResult = rootCommand.Parse(command);
+
+            rootCommand.SetAction(parseResult => 
+            {
+                string parsedFile = parseResult.GetValue(fileOption)??"";
+                Console.WriteLine($"Parsed {parsedFile}");
+            });
+
+            ParseResult parseResult = rootCommand.Parse(command);
+            parseResult.Invoke(); //запуск делегата
+
+        }
+    } 
+    
+}
---
+++ CommandTodoApp/ToDo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommandTodoApp
+{
+    public class ToDo
+    {
+        public string Name { get; set; }// = "Task";
+        public string Description { get; set; }// = "Description";
+        public DateTime CreateAt { get; set; }
+        public DateTime DoneAt { get; set; }
+        public bool IsCompleted { get; set; }
+
+        public ToDo() { }
+        public ToDo(string name, string desc)
+        {
+            this.Name = name;
+            this.Description = desc;
+            CreateAt = DateTime.Now;
+        }
+
+        public ToDo(string name, string description, DateTime createAt, DateTime doneAt, bool isCompleted) : this(name, description)
+        {
+            CreateAt = createAt;
+            DoneAt = doneAt;
+            IsCompleted = isCompleted;
+        }
+    }
+}
