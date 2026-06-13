56bf14da94f92e13311c1570fabe1e3489a56e7f
Shoon <tishinadw@gmail.com>
2026-06-13 16:26:45 +0300

    --add addCommand and options

--- CommandTodoApp/Program.cs
+++ CommandTodoApp/Program.cs
@@ -14,9 +14,25 @@ class Program
     {
         Console.WriteLine("Приложение для задачь. Добавьте задачу");
 
-       
         GetTask();
     }
+
+    //public static void CommandTasks()
+    //{
+    //    while (true) 
+    //    {
+    //        string command = Console.ReadLine() ?? "";
+
+    //        var allOption = new Option<bool>("--all", "Показать все задачи (включая выполненные)");
+
+    //        var addCommand = new Command("add", "Добавление новой задачи") {
+          
+    //        };
+
+    //    }
+    //}
+
+
 
     private static void GetTask()
     {
@@ -24,26 +40,58 @@ class Program
         {
             string command = Console.ReadLine() ?? "";
 
-            Option<string> fileOption = new("--task")
+            Option<string> lastOption = new("--task")
             {
-                Description = "Задача. Пока тестовая команда"
+                Description = "Отобразить задачу с конкретным id"
             };
 
-            RootCommand rootCommand = new("Todo project with System.CommandLine");
-            rootCommand.Options.Add(fileOption);
+            Option<string> allOption = new("--all")
+            {
+                Description = "Вывести лист последних заданий"
+            };
 
+
+            Option<string> titleOption = new("--title")
+            {
+                Description = "Задать название задачи"
+            };
+
+            Option<string> descOptiion = new("--desc") { 
+                Description = "Задать описание задачи"
+            };
+
+            Option<bool> doneOption = new("--done")
+            {
+                Description = "Задача выполнена"
+            };
+
+            RootCommand rootCommand = new("Todo проект с использованием System.CommandLine");
+            // rootCommand.Options.Add(lastOption);
+
+            Command addCommand = new("add", "Создать задачу") //подкоманда
+            {
+                titleOption,
+                descOptiion,
+                doneOption
+            };
+
+            rootCommand.Subcommands.Add(addCommand);
+
+            addCommand.SetAction(parseResult => 
+            Console.WriteLine($" Создаем таск. \n" +
+            $" Название {parseResult.GetValue(titleOption)}\n" +
+            $" Описание {parseResult.GetValue(descOptiion)} \n" +
+            $" Готовность {parseResult.GetValue(doneOption)}"));
 
             //ParseResult parseResult = rootCommand.Parse(command);
 
-            rootCommand.SetAction(parseResult => 
-            {
-                string parsedFile = parseResult.GetValue(fileOption)??"";
-                Console.WriteLine($"Parsed {parsedFile}");
-            });
+            //rootCommand.SetAction(parseResult => 
+            //{
+            //    string parsedFile = parseResult.GetValue(lastOption)??"";
+            //    Console.WriteLine($"Parsed {parsedFile}");
+            //});
 
-            ParseResult parseResult = rootCommand.Parse(command);
-            parseResult.Invoke(); //запуск делегата
-
+            rootCommand.Parse(command).Invoke(); //запуск делегата
         }
     } 
     
